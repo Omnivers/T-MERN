d8378342e1f4a53291185b3ef535caf03a332e55
Omnivers <Twersimor.stock@outlook.com>
2022-11-07 22:22:50 +0100

    get users

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import
 const express = require('express');
 const app=express();
 const mongoose = require('mongoose');
@@ -5,13 +6,14 @@ const dotenv=require('dotenv');
 const userRoute=require('./routes/user');
 const authRoute=require('./routes/auth');
 
+// Config
 dotenv.config();
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log("Hello DBworld")).catch((e)=>{console.log(e)});
 
+// Launch
 app.listen(process.env.PORT,()=>{
     console.log("Hello Server")
 })
-
 app.use("/API/auth", authRoute);
 app.use("/API/user", userRoute);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const User = require("../models/User");
-const {verifyToken,verifyTokenAuth} = require("./verifyToken");
+const {verifyToken,verifyTokenAuth, verifyTokenAdmin} = require("./verifyToken");
 
 const router=require("express").Router();
 
+// EDIT USER
 router.put('/:id',verifyTokenAuth,async(req,res)=>{
     if(req.body.password){
         req.body.password= CryptoJS.AES.encrypt(req.body.password, process.env.PASSCRYPT).toString()
@@ -14,6 +15,37 @@ router.put('/:id',verifyTokenAuth,async(req,res)=>{
         res.status(200).json(updatedUser)
     }catch(e){
         res.status(500).json(e)
+    }
+})
+
+//DELETE USER
+router.delete('/:id',verifyTokenAuth,async(req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted !")
+    }catch(e){
+        res.json(500).json(e)
+    }
+})
+
+//GET USER
+router.get('/find/:id',verifyTokenAdmin,async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        const {password,...others}=user._doc; // to prevent showing the password in our res.json down below
+        res.status(200).json(others);
+    }catch(e){
+        res.json(500).json(e)
+    }
+})
+
+//GET ALL USERS
+router.get('/',verifyTokenAdmin,async(req,res)=>{
+    try{
+        const users = await User.find()
+        res.status(200).json(users);
+    }catch(e){
+        res.json(500).json(e)
     }
 })
 
